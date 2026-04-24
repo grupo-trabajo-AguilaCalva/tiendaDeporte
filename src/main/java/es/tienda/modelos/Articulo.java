da19eabe169a6e7cc12f21b3d3b18f9376974cc2
Pablo_usuario2 <pcastellanocalvano@gmail.com>
2026-04-24 13:37:32 +0200

    He añadido la clase Articulo

---
+++ src/main/java/es/tienda/modelos/Articulo.java
@@ -0,0 +1,50 @@
+package es.tienda.modelos;
+
+public class Articulo {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public Articulo(String codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void altaArticulo(String codigo, String descripcion, double precio) {
+      this.codigo=codigo;
+      this.descripcion=descripcion;
+      this.precio=precio;
+
+    }
+
+    public void visualizarDatos() {
+        System.out.println("ARTÍCULO -> Código: " + codigo +
+                ", Descripción: " + descripcion +
+                ", Precio: " + precio + "€");
+    }
+}
